190bb657bb73ea3e688b6cef12fa510a85a7f09b
Anil Kumar <anil.personal.me@gmail.com>
2023-12-02 15:44:20 +0530

    Intial Commit

---
+++ Authorization using API Key in ASP.NET APIs/Dot Net Core/ApiKeyAuthenticationHandler.cs
@@ -0,0 +1,59 @@
+﻿namespace JWT_R_N_D.Lib
+{
+    using System.Net;
+    // ApiKeyAuthenticationHandler.cs
+    using System.Security.Claims;
+    using System.Text.Encodings.Web;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+
+    public class ApiKeyAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private const string ApiKeyHeader = "Authorization";
+
+        public ApiKeyAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.TryGetValue(ApiKeyHeader, out var apiKey))
+            {
+                Context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                await Context.Response.WriteAsync("API key is missing");
+                return AuthenticateResult.Fail("API key is missing");
+            }
+
+            // Replace the logic below with your own key validation logic
+            if (!IsValidApiKey(apiKey))
+            {
+                // Set a custom response message
+                Context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                await Context.Response.WriteAsync("Invalid API key");
+                return AuthenticateResult.Fail("Invalid API key");
+            }
+
+            var claims = new[] { new Claim(ClaimTypes.Name, "api_user") };
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+
+        private bool IsValidApiKey(string apiKey)
+        {
+            // Implement your API key validation logic here
+            // For example, check against a list of valid keys in your database
+            return apiKey == "your_valid_api_key";
+        }
+    }
+
+}
---
+++ Authorization using API Key in ASP.NET APIs/Dot Net/AuthorizationHeaderHandler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Web;
+using System.Security.Claims;
+using System.Net;
+using System.Web.Http.Results;
+
+namespace WebApplication1.lib
+{
+    public class AuthorizationHeaderHandler
+     : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            IEnumerable<string> apiKeyHeaderValues = null;
+            if (request.Headers.TryGetValues("Authorization", out apiKeyHeaderValues))
+            {
+                var apiKeyHeaderValue = apiKeyHeaderValues.First();
+                if (IsValidApiKey(apiKeyHeaderValue))
+                {
+
+                    var usernameClaim = new Claim(ClaimTypes.Name, "api_user");
+                    var identity = new ClaimsIdentity(new[] { usernameClaim }, "ApiKey");
+                    var principal = new ClaimsPrincipal(identity);
+
+                    HttpContext.Current.User = principal;
+                }
+                else
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.Unauthorized)
+                    {
+                        Content = new StringContent("Invalid API key"),
+                        RequestMessage = request
+                    };
+
+                    return Task.FromResult(response);
+                }
+
+
+
+            }
+            else
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.Unauthorized)
+                {
+                    Content = new StringContent("API key is missing"),
+                    RequestMessage = request
+                };
+
+                return Task.FromResult(response);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+
+
+        private bool IsValidApiKey(string apiKey)
+        {
+            // Implement your API key validation logic here
+            // For example, check against a list of valid keys in your database
+            return apiKey == "your_valid_api_key";
+        }
+    }
+}
